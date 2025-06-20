c6f7b0445f0b1b1587b9e54e3b09eb7391e94372
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2025-06-20 15:34:45 +0900

    feat: Implement Google and email sign-in, sign-up, and password reset functionality

--- app/src/main/java/org/nunocky/myfirebasenoteapp/domain/firebase/FirebaseAuthentication.kt
+++ app/src/main/java/org/nunocky/myfirebasenoteapp/domain/firebase/FirebaseAuthentication.kt
@@ -4,9 +4,14 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import org.nunocky.myfirebasenoteapp.data.User
 import org.nunocky.myfirebasenoteapp.domain.Authentication
+import org.nunocky.myfirebasenoteapp.domain.EmailSignInUseCase
+import org.nunocky.myfirebasenoteapp.domain.GoogleSignInUseCase
 import javax.inject.Inject
 
-class FirebaseAuthentication @Inject constructor() : Authentication {
+class FirebaseAuthentication @Inject constructor(
+    private val googleSignInUseCase: GoogleSignInUseCase,
+    private val emailSignInUseCase: EmailSignInUseCase
+) : Authentication {
     override fun currentUser() = if (Firebase.auth.currentUser == null) null else User(
         uid = Firebase.auth.currentUser?.uid ?: "",
         displayName = Firebase.auth.currentUser?.displayName,
@@ -18,4 +23,16 @@ class FirebaseAuthentication @Inject constructor() : Authentication {
     override fun signOut() {
         Firebase.auth.signOut()
     }
+
+    override suspend fun googleSignIn(googleClientId: String) =
+        googleSignInUseCase.signIn(googleClientId)
+
+    override suspend fun emailSignIn(email: String, password: String) =
+        emailSignInUseCase.signIn(email, password)
+
+    override suspend fun emailSignUp(email: String, password: String) =
+        emailSignInUseCase.signUp(email, password)
+
+    override suspend fun resetPassword(email: String) =
+        emailSignInUseCase.resetPassword(email)
 }
--- app/src/main/java/org/nunocky/myfirebasenoteapp/ui/screens/resetpassword/ResetPasswordViewModel.kt
+++ app/src/main/java/org/nunocky/myfirebasenoteapp/ui/screens/resetpassword/ResetPasswordViewModel.kt
@@ -3,12 +3,10 @@ package org.nunocky.myfirebasenoteapp.ui.screens.resetpassword
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.nunocky.myfirebasenoteapp.domain.Authentication
-import org.nunocky.myfirebasenoteapp.domain.EmailSignInUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class ResetPasswordViewModel @Inject constructor(
     val authentication: Authentication,
-    val emailSignInUseCase: EmailSignInUseCase
 ) : ViewModel() {
 }
